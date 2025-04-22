04d12e99d89d9716605a3761ed8c8436557dbd9e
Empty <make772@ukr.net>
2025-04-22 20:09:45 +0300

    result

--- cinema/views.py
+++ cinema/views.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 
 from django.db.models import F, Count
+from rest_framework.exceptions import NotFound
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.authentication import TokenAuthentication
+from cinema.permissions import (
+    IsAdminOrIfAuthenticatedReadOnly,
+    IsUserAuthenticatedForCreate
+)
 
 from cinema.models import Genre, Actor, CinemaHall, Movie, MovieSession, Order
 
@@ -24,21 +30,66 @@ from cinema.serializers import (
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def retrieve(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def update(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def partial_update(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def destroy(self, request, *args, **kwargs):
+        raise NotFound()
 
 
 class ActorViewSet(viewsets.ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def retrieve(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def update(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def partial_update(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def destroy(self, request, *args, **kwargs):
+        raise NotFound()
 
 
 class CinemaHallViewSet(viewsets.ModelViewSet):
     queryset = CinemaHall.objects.all()
     serializer_class = CinemaHallSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+
+    def retrieve(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def update(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def partial_update(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def destroy(self, request, *args, **kwargs):
+        raise NotFound()
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.prefetch_related("genres", "actors")
     serializer_class = MovieSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+    http_method_names = ["get", "post"]
 
     @staticmethod
     def _params_to_ints(qs):
@@ -87,6 +138,8 @@ class MovieSessionViewSet(viewsets.ModelViewSet):
         )
     )
     serializer_class = MovieSessionSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_queryset(self):
         date = self.request.query_params.get("date")
@@ -124,6 +177,20 @@ class OrderViewSet(viewsets.ModelViewSet):
     )
     serializer_class = OrderSerializer
     pagination_class = OrderPagination
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsUserAuthenticatedForCreate,)
+
+    def retrieve(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def update(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def partial_update(self, request, *args, **kwargs):
+        raise NotFound()
+
+    def destroy(self, request, *args, **kwargs):
+        raise NotFound()
 
     def get_queryset(self):
         return Order.objects.filter(user=self.request.user)
